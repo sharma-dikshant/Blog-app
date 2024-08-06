4afc688159101765afa4c4b3765548f414cd5be8
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-06 17:04:50 +0530

    setup environment variables

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { config } from "./config/config";
+
+function App() {
+ console.log(config);
+  return (
+    <div>
+      <h1>hello workd</h1>
+    </div>
+  );
+}
+
+export default App;
